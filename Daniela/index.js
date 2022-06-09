4a51910a17d38cabfd59b585a2819f52e3f6e25b
ajnacodes <b.daniela196@outlook.com>
2022-06-09 11:00:24 +0200

    finish basic1

--- Daniela/index.js
+++ Daniela/index.js
@@ -1,1 +1,16 @@
 "use strict";
+// Basic 1:
+// Create a class constructor named Person that should hold information about name, age,  jobTitle and have a function that will return a string “Hello there, My name is (name) and I am (age) years old, and I am a (jobTitle)”
+class Person {
+    constructor(fname, lname, age, jobTitle) {
+        this.fname = fname;
+        this.lname = lname;
+        this.age = age;
+        this.jobTitle = jobTitle;
+    }
+    printSentence() {
+        return `Hello there, my name is ${this.fname} ${this.lname} and I am ${this.age} years old, and I am a ${this.jobTitle}.`;
+    }
+}
+let person1 = new Person("Anna", "Smith", 25, "Web Designer");
+console.log(person1.printSentence());
